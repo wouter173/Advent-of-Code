52d23facbdc330bf30b2c4ef96edfbb17fca55d1
wouter <wouter@debruijn.dev>
2021-12-13 15:52:35 +0100

    🎄day 13, but a bit prettier bc it deserves more 🎄

--- 2021/#13/p1.js
+++ 2021/#13/p1.js
@@ -7,17 +7,12 @@ const instructions = data
 	.slice(data.indexOf('')+1, data.length)
 	.map(i => i.split('fold along ')[1].split('=').map((a, j) => j == 1? parseInt(a): a))
 
-
-const instruction = instructions[0]
-if (instruction[0] == 'x') {
-	dots = dots.map(d => [d[0] > instruction[1]? instruction[1] - (d[0] - instruction[1]): d[0], d[1]])
-} 
-if (instruction[0] == 'y') {
-	dots = dots.map(d => [d[0], d[1] > instruction[1]? instruction[1] - (d[1] - instruction[1]): d[1]])
-}
+const ins = instructions[0]
+ins[0] == 'x'? 
+	dots = dots.map(d => [d[0] > ins[1]? ins[1] * 2 - d[0]: d[0], d[1]]):
+	dots = dots.map(d => [d[0], d[1] > ins[1]? ins[1] * 2 - d[1]: d[1]])
 
 const max = [Math.max(...dots.reduce((acc, cur) => [...acc, cur[0]], [])), Math.max(...dots.reduce((acc, cur) => [...acc, cur[1]], [])), ]
 const paper = new Array(max[1]+1).fill(null).map(() => new Array(max[0]+1).fill('.'))
 dots.map(d => paper[d[1]][d[0]] = '#')
-paper.map(i => console.log(i.join('')))
 console.log(paper.reduce((acc, cur) => [...acc, ...cur], []).filter(i => i == '#').length)
--- 2021/#13/p2.js
+++ 2021/#13/p2.js
@@ -7,16 +7,13 @@ const instructions = data
 	.slice(data.indexOf('')+1, data.length)
 	.map(i => i.split('fold along ')[1].split('=').map((a, j) => j == 1? parseInt(a): a))
 
-for(let instruction of instructions) {
-	if (instruction[0] == 'x') {
-		dots = dots.map(d => [d[0] > instruction[1]? instruction[1] - (d[0] - instruction[1]): d[0], d[1]])
-	} 
-	if (instruction[0] == 'y') {
-		dots = dots.map(d => [d[0], d[1] > instruction[1]? instruction[1] - (d[1] - instruction[1]): d[1]])
-	}
-}
+instructions.map(ins => {
+	ins[0] == 'x'? 
+		dots = dots.map(d => [d[0] > ins[1]? ins[1] * 2 - d[0]: d[0], d[1]]):
+		dots = dots.map(d => [d[0], d[1] > ins[1]? ins[1] * 2 - d[1]: d[1]])
+})
 
 const max = [Math.max(...dots.reduce((acc, cur) => [...acc, cur[0]], [])), Math.max(...dots.reduce((acc, cur) => [...acc, cur[1]], [])), ]
-const paper = new Array(max[1]+1).fill(null).map(() => new Array(max[0]+1).fill('.'))
+const paper = new Array(max[1]+1).fill(null).map(() => new Array(max[0]+1).fill(' '))
 dots.map(d => paper[d[1]][d[0]] = '#')
 paper.map(i => console.log(i.join('')))
