aaa32ff065d9f2c17bb1a02bf6fd5062f007aad3
wouter <wouter@debruijn.dev>
2022-12-01 14:51:47 +0100

    day 1 but less retarded

--- 2022/01/p1.ts
+++ 2022/01/p1.ts
@@ -2,6 +2,6 @@ const inputs = await Deno.readTextFile("./input.txt");
 
 const y = inputs
   .split("\n\n")
-  .map((l) => l.split("\n").reduce((cur, acc) => Number(cur) + Number(acc), 0));
+  .map((l) => l.split("\n").reduce((acc, cur) => acc + +cur, 0));
 
 console.log(Math.max(...y));
--- 2022/01/p2.ts
+++ 2022/01/p2.ts
@@ -2,7 +2,7 @@ const inputs = await Deno.readTextFile("./input.txt");
 
 const y = inputs
   .split("\n\n")
-  .map((l) => l.split("\n").reduce((cur, acc) => Number(cur) + Number(acc), 0));
+  .map((l) => l.split("\n").reduce((acc, cur) => acc + +cur, 0));
 
 const topThree = y.sort((a, b) => b - a).slice(0, 3);
 
